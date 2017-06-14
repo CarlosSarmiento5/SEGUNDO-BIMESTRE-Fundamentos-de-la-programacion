4c3a4fa283d6d1b94b6767f94c2ad1588cb34b93
CarlosSarmiento5 <carlosrokylukycsc@hotmail.com>
2017-06-14 09:54:35 -0500

    Add files via upload

---
+++ ACTIVIDADGRUPAL,14-06-17/Clase 7 Ejercicio 2/src/clase/pkg7/ejercicio/pkg2/Clase7Ejercicio2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase.pkg7.ejercicio.pkg2;
+
+import java.util.Scanner;
+public class Clase7Ejercicio2 {
+
+    private int[][] mat;
+    private Scanner entrada;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
